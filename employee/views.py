776b661b255c30982a57e812a71d2eff7e2d5476
Mayun-binod <shresthabinod334@gmail.com>
2024-10-07 23:15:35 +0545

    added

--- employee/views.py
+++ employee/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from employee.forms import EmployeeForm
 # Create your views here.
 
 def index(request):
@@ -8,19 +9,38 @@ def index(request):
 
 
 def employee(request):
-    # return HttpResponse("this is employee page")
-    if request.method == 'POST':  # Correcting 'POSt' to 'POST'
-        data = request.POST
-        email = data['email']
-        password = data['password']
-        context = {
-            'name': email,
-            'address': password,
-        }
+    if request.method=='POST':
+        pass
+        # data=request.POST
+        # first_name=data['first_name']
+        # print(first_name)
+        # last_name=data['last_name']
+        # print(last_name)
+        # address=data['address']
+        # print(address)
+        # context={
+        #     'first_name':first_name,
+        #     'last_name':last_name,
+        #     'address':address,
+        # }
     else:
-        context = {
-            'name': "Binod",
-            'address': "Lalitpur",
+        form=EmployeeForm()
+        context={
+            'form':form
         }
+    # return HttpResponse("this is employee page")
+    # if request.method == 'POST':  # Correcting 'POSt' to 'POST'
+    #     data = request.POST
+    #     email = data['email']
+    #     password = data['password']
+    #     context = {
+    #         'name': email,
+    #         'address': password,
+    #     }
+    # else:
+    #     context = {
+    #         'name': "Binod",
+    #         'address': "Lalitpur",
+    #     }
 
     return render(request, 'employee/employee.html', context)
